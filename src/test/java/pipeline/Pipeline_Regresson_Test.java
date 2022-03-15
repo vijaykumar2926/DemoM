9ed149114f258a27c527fad30e6c16b62c88be5d
vijay <vijay@192.168.100.9>
2022-03-15 20:10:09 +0530

    Pushing using master

---
+++ src/test/java/pipeline/Pipeline_Regresson_Test.java
@@ -0,0 +1,14 @@
+package pipeline;
+
+import org.testng.annotations.Test;
+
+public class Pipeline_Regresson_Test 
+{
+	@Test
+	public void pipeline_regression()
+	{
+		System.out.println("========================");
+		System.out.println(" SMOKE REGRESSION SUITE ");
+		System.out.println("========================");
+	}
+}
