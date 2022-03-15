bc855502dab210d00e93ac28d9eaef7000f8f99c
vijay <vijay@192.168.100.9>
2022-03-15 20:20:04 +0530

    pushing from master

--- src/test/java/pipeline/Pipeline_Build_Test.java
+++ src/test/java/pipeline/Pipeline_Build_Test.java
@@ -2,10 +2,10 @@ package pipeline;
 
 import org.testng.annotations.Test;
 
-public class Pipelin_Build_Test 
+public class Pipeline_Build_Test 
 {
 	@Test
-	public void buildtest()
+	public void pipeline_buildtest()
 	{
 		System.out.println("==================");
 		System.out.println(" BUILD CREATION ");
