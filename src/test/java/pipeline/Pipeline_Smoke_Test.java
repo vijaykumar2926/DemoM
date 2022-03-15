c40bb7f284af0f445dcec1d6dcb463cc88a15311
vijay <vijay@192.168.100.9>
2022-03-15 19:58:12 +0530

    created in branch1 = pushing as master

---
+++ src/test/java/pipeline/Pipeline_Smoke_Test.java
@@ -0,0 +1,14 @@
+package pipeline;
+
+import org.testng.annotations.Test;
+
+public class Pipeline_Smoke_Test 
+{
+	@Test
+	public void pipeline_Smoke()
+	{
+		System.out.println("==================");
+		System.out.println(" SMOKE TEST SUITE ");
+		System.out.println("==================");
+	}
+}
