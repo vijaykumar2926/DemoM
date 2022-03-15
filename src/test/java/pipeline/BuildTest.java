946b1281be722fae0a8f6ff032df40fdd1024e98
vijay <vijay@192.168.100.9>
2022-03-15 19:50:47 +0530

    pushing using branch1

---
+++ src/test/java/pipeline/BuildTest.java
@@ -0,0 +1,14 @@
+package pipeline;
+
+import org.testng.annotations.Test;
+
+public class BuildTest 
+{
+	@Test
+	public void buildtest()
+	{
+		System.out.println("==================");
+		System.out.println(" BUILD CREATION ");
+		System.out.println("==================");
+	}
+}
