c62ca756cd45f1b5c0dddb0a24447023bc4a5384
vijay <vijay@LAPTOP-0SCJ6J7A>
2022-03-16 10:48:52 +0530

    dasgcdhifaidj

---
+++ src/test/java/ParameterPractice/ReadTest.java
@@ -0,0 +1,18 @@
+package ParameterPractice;
+
+import org.testng.annotations.Test;
+
+public class ReadTest 
+{
+	@Test
+	public void test()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+
+
+	}
+}
